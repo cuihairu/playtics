2adb84a54f25381d738e1519bca82f2ceadcba53
cuihairu <chuihairu@gmail.com>
2025-10-25 07:01:59 +0800

    control: include experiment config JSON Schema in resources

--- services/control-service/build.gradle.kts
+++ services/control-service/build.gradle.kts
@@ -8,6 +8,8 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-web")
   implementation("org.springframework.boot:spring-boot-starter-actuator")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+  // JSON Schema validation for experiment config
+  implementation("com.networknt:json-schema-validator:1.0.91")
   runtimeOnly("com.h2database:h2")
 }
 
